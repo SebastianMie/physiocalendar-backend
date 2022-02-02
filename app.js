bf6879f4f9ff79569db5b8a89ee0999f04f4549b
astrutz <--replace-all>
2022-02-02 12:35:43 +0100

    Save data on PUT request

--- app.js
+++ app.js
@@ -4,20 +4,22 @@ const fs = require('fs');
 const app = express();
 const PORT = 4000;
 
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(cors());
 
 app.get('/backup', (req, res) => {
   const data = JSON.parse(fs.readFileSync(__dirname + '/data/backup.json'));
   res.send(data);
 });
 
-app.put('/backup', () => {
-
+app.put('/backup', (req, res) => {
+  const postData = req.body;
+  fs.writeFileSync(__dirname + '/data/backup.json', JSON.stringify(postData, null, 2));
+  res.sendStatus(200);
 });
 
 
