823e7f9242f59fd44bec725391915b078451bbb6
astrutz <--replace-all>
2022-11-18 10:00:45 +0100

    Increase request limit

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ const fs = require('fs');
 const app = express();
 const PORT = 4000;
 
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({
-  extended: true
+  extended: true,
+  limit: '50mb'
 }));
 
 app.use(cors());
